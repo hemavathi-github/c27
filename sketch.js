d674fb17e883fe06d654ed6b928058a97ae5294a
hemavathi-github <hema15sg@gmail.com>
2021-01-07 13:53:17 +0530

    add chain

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var birdlog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -18,12 +20,13 @@ function setup(){
 
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
+    
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
     log1 = new Log(810,260,300, PI/2);
-
+    
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
@@ -35,6 +38,17 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+    birdlog = new Log(230, 180, 80, PI/2);
+
+    var options = {
+        bodyA: bird.body,
+        bodyB: birdlog.body,
+        stiffness: 0.04,
+        length:10,
+    }
+    var chain= Constraint.create(options);
+    World.add(world,chain);
+    
 
 }
 
@@ -58,7 +72,11 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    
     bird.display();
     platform.display();
+    birdlog.display();
+
+    strokeWeight(5);
+    line(bird.body.position.x,bird.body.position.y,birdlog.body.position.x,birdlog.body.position.y);
 }
